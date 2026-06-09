574923b90f9c2f003e883d5f8d2694f98e1d8aa6
Devansh Jain <jdevansh597@gmail.com>
2026-06-09 16:58:16 +0530

    tcp socket opened and 3-way handshake established, bytes read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    // buffer
+    let mut buffer = [0; 1024];
+    // copying raw bytes
+    stream.read(&mut buffer)?;
+    println!("{}", String::from_utf8_lossy(&buffer));
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    // open socket, bind and listen
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+
+    // accept established connections
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+
+    Ok(())
 }
