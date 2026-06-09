be8bd1d5dd77fd0feafe60757af4706a8904128c
devanshjain15 <jdevansh597@gmail.com>
2026-06-09 20:53:08 +0530

    Sending html response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
@@ -6,7 +6,15 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer = [0; 1024];
     // copying raw bytes
     stream.read(&mut buffer)?;
-    println!("{}", String::from_utf8_lossy(&buffer));
+    // println!("{}", String::from_utf8_lossy(&buffer));
+
+    let status = "HTTP/1.1 200 OK"; 
+    let contents = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Hello World</title>\n</head>\n<body>\n<h1>Hello from the server!</h1>\n</body>\n</html>";
+    let content_len = contents.len(); 
+    let response = format!(
+        "{status}\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {content_len}\r\n\r\n{contents}"
+    );
+    stream.write_all(response.as_bytes())?; 
     Ok(())
 }
 
