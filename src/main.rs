ca298b40e0e52ea0aa585813c6e169cefa65000b
devanshjain15 <jdevansh597@gmail.com>
2026-06-12 16:29:35 +0530

    added Request and Response struct and stream handled internally + parsing headers

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,19 @@
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
-// struct Request { }
-// struct Response { }
-
-fn parse_request_line(request: &str) -> (&str, &str, &str) {
-    // method, path, version
-    let line = request.lines().next().unwrap();
-    let first_line: Vec<&str> = line.split_whitespace().collect();
-    (first_line[0], first_line[1], first_line[2])
+struct Request {
+    pub method: String,
+    pub path: String,
+    pub version: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
 
-fn router(
-    method: &str,
-    path: &str,
-    routes: &HashMap<String, fn(&str, &str) -> (u32, String, String)>,
-) -> (u32, String, String) {
-    let k = format!("{}#{}", method, path);
-    if routes.contains_key(&k) {
-        let handler = routes.get(&k).unwrap();
-        handler(method, path)
-    } else {
-        (404, "Not Found".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Not Found</title>\n</head>\n<body>\n<h1>404 Not Found</h1>\n<p>The requested resource was not found on this server.</p>\n</body>\n</html>\n".to_string())
-    }
-}
-
-fn handle_root(_method: &str, _path: &str) -> (u32, String, String) {
-    (200, "OK".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Hello World</title>\n</head>\n<body>\n<h1>Hello from the server!</h1>\n</body>\n</html>\n".to_string())
-}
-
-fn handle_about(_method: &str, _path: &str) -> (u32, String, String) {
-    (200, "OK".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>About</title>\n</head>\n<body>\n<h1>About Page</h1>\n<p>This is a simple HTTP server implemented in Rust.</p>\n</body>\n</html>\n".to_string())
-}
-
-fn main() -> std::io::Result<()> {
-    // open socket, bind and listen
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    println!("The server is up at 127.0.0.1:8000");
-
-    // routing stuff
-    let mut routes: HashMap<String, fn(&str, &str) -> (u32, String, String)> = HashMap::new();
-    routes.insert("GET#/".to_string(), handle_root);
-    routes.insert("GET#/about".to_string(), handle_about);
-
-    // accept established connections
-    for stream in listener.incoming() {
+impl Request {
+    pub fn new(stream: &TcpStream) -> Self {
         // copying and reading incoming raw bytes in chunks
-        let mut reader = BufReader::new(stream.as_ref().unwrap());
+        let mut reader = BufReader::new(stream);
         let mut buffer: Vec<String> = Vec::new();
 
         loop {
@@ -59,39 +25,114 @@ fn main() -> std::io::Result<()> {
             buffer.push(chunk.trim_end().to_string());
         }
 
-        // parsing headers and checking for Content-Length
-        let mut content_length = 0;
-        for chunk in buffer.iter() {
-            if chunk.contains("Content-Length:") {
-                let header: Vec<&str> = chunk.split(' ').collect::<Vec<&str>>();
-                content_length = header[1].parse().unwrap();
-                break;
-            }
-        }
-        println!("{}", content_length);
+        // parsing headers
+        let mut headers = HashMap::new(); 
 
+        // determining content-length
+        let mut content_length = 0;
+        for (i, chunk) in buffer.iter().enumerate() {
+            if i == 0 { 
+                continue; 
+            }
+
+            let header: Vec<&str> = chunk.splitn(2, ':').collect(); 
+            if header[0] == "Content-Length" {
+                content_length = header[1].trim().parse().unwrap();
+            }
+            headers.insert(header[0].to_string(), header[1].trim().to_string()); 
+        }
+        
+        let mut body = String::new(); 
         if content_length != 0 {
-            // find the end of the request based on the Content-Length
+            // finding the end of the request based on the Content-Length
             let mut body_buf = vec![0; content_length];
             reader.read_exact(&mut body_buf).unwrap();
-            buffer.push(String::from_utf8_lossy(&body_buf).into_owned());
+            body = String::from_utf8_lossy(&body_buf).to_string();
         }
+        
+        // determining method, path, version
+        let request_line: Vec<&str>= buffer[0].split_whitespace().collect(); 
 
-        let request = buffer.join("\n");
-        println!("{request}");
+        Self {
+            method: request_line[0].to_string(), 
+            path: request_line[1].to_string(), 
+            version: request_line[2].to_string(), 
+            headers, 
+            body: Some(body)
+        }
+    }
+}
 
-        let (method, path, version) = parse_request_line(&request); 
-        println!("{method}, {path}, {version}");
+struct Response {
+    version: String,
+    pub status: u32,
+    pub status_description: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
+}
 
-        let (status_code, status_text, body) = router(method, path, &routes);
-        let content_len = body.len();
+impl Response { 
+    pub fn new(version: String, status: u32, status_description: String, body: String) -> Self{ 
+        Self { 
+            version,
+            status, 
+            status_description, 
+            headers: HashMap::new(), 
+            body: Some(body)
+        }
+    }
 
-        // sending http response
-        let status = format!("{version} {status_code} {status_text}");
-        let response = format!(
-            "{status}\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {content_len}\r\n\r\n{body}"
-        );
-        stream.unwrap().write_all(response.as_bytes())?;
+    fn write_all(&self,mut stream: &TcpStream) -> std::io::Result<()>{ 
+        let (content_length, content) = match &self.body { 
+            Some(content) => (content.len(), content), 
+            None => (0, &"".to_string())
+        };
+        let response_string = format!("{} {} {}\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {}\r\n\r\n{}", self.version, self.status, self.status_description, content_length, content);
+        stream.write_all(response_string.as_bytes())
+    }
+}
+
+fn router(
+    request: &Request,
+    routes: &HashMap<String, fn(&Request) -> Response>,
+) -> Response {
+    let k = format!("{}#{}", request.method, request.path);
+    if routes.contains_key(&k) {
+        let handler = routes.get(&k).unwrap();
+        handler(request)
+    } else {
+        Response::new(request.version.to_string(),404, "Not Found".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Not Found</title>\n</head>\n<body>\n<h1>404 Not Found</h1>\n<p>The requested resource was not found on this server.</p>\n</body>\n</html>\n".to_string())
+    }
+}
+
+fn handle_root(request: &Request) -> Response {
+    Response::new(request.version.to_string(), 200, "Ok".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Hello World</title>\n</head>\n<body>\n<h1>Hello from the server!</h1>\n</body>\n</html>\n".to_string())
+}
+
+fn handle_about(request: &Request) -> Response {
+    Response::new(request.version.to_string(), 200, "OK".to_string(), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>About</title>\n</head>\n<body>\n<h1>About Page</h1>\n<p>This is a simple HTTP server implemented in Rust.</p>\n</body>\n</html>\n".to_string())
+}
+
+fn main() -> std::io::Result<()> {
+    // open socket, bind and listen
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    println!("The server is up at 127.0.0.1:8000");
+
+    // routing stuff
+    let mut routes: HashMap<String, fn(&Request) -> Response> = HashMap::new();
+    routes.insert("GET#/".to_string(), handle_root);
+    routes.insert("GET#/about".to_string(), handle_about);
+
+    // accept established connections
+    for stream in listener.incoming() {
+        let stream = stream.unwrap(); 
+
+        // reading + parsing request
+        let request = Request::new(&stream); 
+        // routing
+        let response = router(&request, &routes);
+        // responding 
+        response.write_all(&stream)?;
     }
 
     Ok(())
